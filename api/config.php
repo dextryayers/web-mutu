a2e112707eb3c3a75a7a24c821baffd5648a1f79
Aniip_01 <142471909+dextryayers@users.noreply.github.com>
2025-12-13 19:48:22 +0700

    Delete api directory

--- api/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
-define('DB_NAME', getenv('DB_NAME') ?: 'DB_NAME');
-define('DB_USER', getenv('DB_USER') ?: 'DB_USER');
-define('DB_PASS', getenv('DB_PASS') ?: 'DB_PASS');
-define('BASE_URL', getenv('BASE_URL') ?: 'https://DOMAIN/api');
-define('ADMIN_SECRET_CODE', 'ADMIN_CODE');
